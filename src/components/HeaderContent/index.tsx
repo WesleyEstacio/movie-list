1d25bf652c612503d8455080991ea0b142b5550a
Wesley Estacio <wesleyestacio4@gmail.com>
2022-05-16 11:49:23 -0300

    feat Header Content

--- src/components/HeaderContent/index.tsx
+++ src/components/HeaderContent/index.tsx
@@ -6,7 +6,29 @@ export function HeaderContent() {
             <Content>
                 <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
 
-                <p>FILTRE POR:</p>
+                <article>
+                    <p>FILTRE POR:</p>
+
+                    <button>Ação</button>
+                    <button>Aventura</button>
+                    <button>Animação</button>
+                    <button>Comédia</button>
+                    <button>Crime</button>
+                    <button>Documentário</button>
+                    <button>Drama</button>
+                    <button>Família</button>
+                    <button>Fantasia</button>
+                    <button>História</button>
+                    <button>Terror</button>
+                    <button>Música</button>
+                    <button>Mistério</button>
+                    <button>Romance</button>
+                    <button>Ficção científica</button>
+                    <button>Cinema TV</button>
+                    <button>Thriller</button>
+                    <button>Guerra</button>
+                    <button>Faroeste</button>
+                </article>
             </Content>
         </Container>  
     )
--- src/components/HeaderContent/styles.tsx
+++ src/components/HeaderContent/styles.tsx
@@ -9,7 +9,7 @@ export const Content = styled.div`
     margin: 0 auto;
     min-height: 450px;
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     flex-direction: column;
 
@@ -19,11 +19,25 @@ export const Content = styled.div`
         text-align: center;
     }
 
-    p {
-        font-size: 14px;
-        color: #fff;
-        font-weight: bold;
-        letter-spacing: 0.1rem;
-        margin-top: 2rem;
+    article {
+        text-align: center;
+
+        p {
+            font-size: 14px;
+            color: #fff;
+            font-weight: bold;
+            letter-spacing: 0.1rem;
+            padding: 0.5rem;
+        }
+
+        button {
+            border: 0;
+            background-color: transparent;
+            font-weight: bold;
+            background-color: #fff;
+            padding: 0.7rem 1rem;
+            border-radius: 0.2rem;
+            margin: 0.5rem;
+        }
     }
 `
