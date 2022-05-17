c015a7d81cb01ab24977773cfb7f9f8cf35adfe9
Wesley Estacio <wesleyestacio4@gmail.com>
2022-05-17 14:34:43 -0300

    feat get api

--- src/components/FilmList/index.tsx
+++ src/components/FilmList/index.tsx
@@ -2,6 +2,14 @@ import { FilmItem } from "../FilmItem";
 import { Container } from "./styles";
 
 export function FilmList() {
+
+    function handleFetchApi() {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=c908cc361daab221ef316ddff3c6e5dc')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+
+    
     return (
         <Container>
             <FilmItem />
@@ -28,6 +36,7 @@ export function FilmList() {
             <FilmItem />
             <FilmItem />
             <FilmItem />
+            <button onClick={handleFetchApi}>Teeste</button>
         </Container>
     )
 }
